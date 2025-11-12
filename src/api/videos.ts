1bd18953e634c77512f6e581913831e2e354e1a7
rawangba <richardannyongw@gmail.com>
2025-11-11 20:33:46 -0800

    Upload videos to S3 bucket

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,105 @@
 import { respondWithJSON } from "./json";
+import { rm } from "fs/promises";
+import path from "path";
 
 import { type ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 
+import { fileTypeToExt } from "./assets";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo, updateVideo } from "../db/videos";
+import { uploadVideoToS3 } from "../s3";
+
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
-  return respondWithJSON(200, null);
+  // Extract videoID from URL path parameter, parse as UUID
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  // Authenticate user
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  // Get video metadata from SQLite database
+  const videoMetadata = await getVideo(cfg.db, videoId);
+  
+  // Check if provided userID matches userID of the video in the DB
+  if (!videoMetadata || userID !== videoMetadata.userID) {
+    throw new UserForbiddenError(`Video owner ${userID} does not match`);
+  }
+
+  console.log("uploading video", videoId, "by user", userID);
+
+  // Parse form data and get video from the form
+  const formData = await req.formData();
+  const file = formData.get("video");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Video file missing");
+  }
+
+  // Check file size of video
+  const MAX_UPLOAD_SIZE = 1 << 30;
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("File too large, max size is 1GB");
+  }
+
+  // Get media type of video
+  const fileType = file.type;
+  if (fileType !== "video/mp4") {
+    throw new BadRequestError("Thumbnail must be JPEG or PNG");
+  }
+
+  // create a temporary file on the disk
+  const tempFileName = path.join("/tmp", `${videoId}.mp4`);
+  const tempFile = Bun.file(tempFileName);
+  
+  // Write video to temporary file 
+  await Bun.write(tempFile, file);
+
+  // Generate S3 file key
+  //const { randomBytes } = await import('node:crypto');
+  //const buf = randomBytes(32);
+  //const fileKey = `${buf.toString('base64url')}${fileTypeToExt(fileType)}`;
+  const fileKey = `${videoId}${fileTypeToExt(fileType)}`;
+  console.log(`S3 File Key is ${fileKey}`);
+
+  // Write to S3
+  await uploadVideoToS3(cfg, fileKey, tempFileName, fileType);
+
+  // Generate S3 URL
+  const videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${fileKey}`
+  console.log(`Video uploaded to ${videoURL}`);
+
+  // Update video metadata in the DB to use the video URL
+  videoMetadata.videoURL = videoURL;
+  updateVideo(cfg.db, videoMetadata);
+
+  // Delete temporary file
+  await Promise.all([rm(tempFileName, { force: true })]);
+
+  // Respond with updated JSON of video's metadata 
+  return respondWithJSON(200, videoMetadata);
 }
+
+function getVideoAspectRatio(filePath: string): string {
+  const proc = Bun.spawn(["ffprobe", "-v", "error" "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "json", filePath], {
+    stderr: "pipe",
+  });
+  await proc.exited;
+  if (proc.exitCode !== 0) {
+    console.error("Bad exit");
+  }
+
+  const errors: string = await proc.stderr.text();
+  if (errors) {
+    console.error(errors);
+  }
+  const text = await proc.stdout.text();
+  console.log(text);
+
+  return "other";
+}
+
